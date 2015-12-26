817853ebcbbe1592434cfd00068d0630831b6117
Chengzhong Wu <chengzhong.wu@redhome.cc>
2015-12-26 11:35:33 +0800

    fix type name

--- hdu-oauth2-client.d.ts
+++ hdu-oauth2-client.d.ts
@@ -6,20 +6,20 @@ declare module 'hdu-oauth2-client' {
     import { IncomingMessage, ServerRequest, ServerResponse } from 'http';
     
     interface ClientOptions {
-        id: String,
-        secret: String,
-        apiHost?: String,
-        protocol?: String,
+        id: string,
+        secret: string,
+        apiHost?: string,
+        protocol?: string,
     }
     
     interface ProxyConfig {
-        prefix: String
+        prefix: string
     }
     
     export class Client {
         constructor(options: ClientOptions);
         
-        get(resourcePath: String, callback?: (error: any, response: IncomingMessage, body: any) => void);
+        get(resourcePath: string, callback?: (error: any, response: IncomingMessage, body: any) => void);
         
         proxy(config: ProxyConfig): (req: ServerRequest, res: ServerResponse, next: (error?: any) => void) => void;
     }
