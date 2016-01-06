4ff39d2934cb6ef5257420a792254311c675dcfb
XGHeaven <619943612@qq.com>
2016-01-06 17:52:30 +0800

    add shorthand for exam

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /**
  * Created by rabbitsion on 7/21/15.
  */
-var Client = require('./lib/api.js');
+var Client = require('./lib/api');
+
+Client.mixed(require('./lib/exam'));
 
 module.exports = Client;
