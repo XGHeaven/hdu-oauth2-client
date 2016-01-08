11361dc051672522696c405f1bd3dc7a72895f3b
XGHeaven <619943612@qq.com>
2016-01-08 11:22:41 +0800

    fix: util.highOrderForId return function without return

--- test/spec/util.js
+++ test/spec/util.js
@@ -2,16 +2,20 @@
 
 const muk = require('muk');
 const util = require('../../lib/util');
+const Promise = require('bluebird');
 
 describe('util', function() {
-    it('highOrderForId', function() {
+    it('highOrderForId', function(done) {
         let temp = {
             getAsync(url) {
-                url.should.be.eql('/path/to/exec/id')
+                return new Promise((resolve, reject) => {
+                    if (url instanceof Error) return reject(url);
+                    resolve(url);
+                })
             }
         }
         
         temp.func = util.highOrderForId('/path/to/exec');
-        temp.func('id');
+        temp.func('id').should.be.become('/path/to/exec/id').notify(done);
     })
 })
